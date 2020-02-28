f99ccccedb730ce98232e6bf5e93819124610980
François Desrichard <francois.desrichard@irit.fr>
2020-02-28 15:20:29 +0100

    Fuse main.cpp and main_window.h

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
-#include "main_window.h"
+#include "layout.h"
 
 #include <QApplication>
+#include <QLabel>
+#include <QLineEdit>
+#include <QMainWindow>
+#include <QPushButton>
+
+#include <memory>
+
+
+class MainWindow : public QMainWindow {
+public:
+    MainWindow() {
+        using Input = qtl::HBox<QLabel, QLineEdit>;
+        using Form  = qtl::VBox<Input, Input, QPushButton>;
+
+        auto form = std::make_unique<Form>();
+        form->child<0, 0>()->setText("First name:");
+        form->child<1, 0>()->setText("Last name:");
+        form->child<2>()->setText("Confirm");
+
+        setCentralWidget(form.release());
+        show();
+    }
+};
 
 
 int main(int argc, char* argv[]) {
